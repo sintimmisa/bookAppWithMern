3e4e305e83ad7a63f8d016b5c4d24344d48e8225
sintimmisa <sintim.misa@gmail.com>
2020-08-25 03:03:46 +0000

    Added ReactJS to MERN stack starter

---
+++ client/src/Component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import("../Styles/App.css");
+
+class App extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <h1>Welcome to My MERN Stack Starter</h1>
+            </div>
+        )
+    }
+};
+
+export default App;
