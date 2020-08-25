3316f8f43b310efc2b7893715e7f357397262fa5
sintimmisa <sintim.misa@gmail.com>
2020-08-25 08:46:02 +0000

    Fixed Babel Error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,32 +2,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    //Set entry and ouput dir of app
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
-    },
-    //
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude:/nodule_modules/,
-                use: {
-                    loader:"babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use:["style-loader","css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template:"./src/index.html"
-    })
-    ]
-    
+  //Set entry and ouput dir of app
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  //
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /nodule_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/Index.html",
+    }),
+  ],
 };
